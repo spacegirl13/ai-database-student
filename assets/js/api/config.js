d6547cc627f83ed236e0c72f0b4d34bb64d2f5de
SpaceGirl13 <anishkasanghvi@gmail.com>
2026-01-16 13:44:50 -0800

    changed backend subdomain

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -7,7 +7,7 @@ export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     pythonURI = "http://localhost:8402";  // Same URI for localhost or 127.0.0.1
 } else {
-    pythonURI = "https://aiforstudents.opencodingsociety.com";
+    pythonURI = "https://aistudybuddy.opencodingsociety.com";
 
 }
 
